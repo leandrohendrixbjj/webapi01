da1245d083780c31d4fed6805989b0663a386070
hendrix <leandrotully@gmail.com>
2023-01-21 08:26:41 -0300

    Primeira WebAPI

---
+++ db.js
@@ -0,0 +1,40 @@
+const { v4 } = require('uuid');
+
+global.users = [];
+
+function findOne(id) {
+    return global.users.find(item => item.id == id);
+}
+
+function all() {
+    return global.users;
+}
+
+function store(user) {
+    user.id = v4(); //v4 versão da uuid
+    global.users.push(user);
+}
+
+function update(id, user) {
+    return global.users.forEach((item, index, array) => {
+        if (item.id === id) {
+            user.id = id;
+            array[index] = user;
+        }
+    });
+}
+
+function remove(id) {
+    return global.users.forEach((item, index, array) => {
+        if (item.id == id)
+            array.splice(index, 1)
+    })
+}
+
+module.exports = {
+    findOne,
+    all,
+    store,
+    update,
+    remove
+}
